ab917568e4d52c1cf9dd1086e830a018adf9a9d7
Ras96 <asymptote.k.k@gmail.com>
2021-12-02 09:42:25 +0900

    :green_heart: Add handler tests

--- interfaces/handler/alias_test.go
+++ interfaces/handler/alias_test.go
@@ -1,16 +1,19 @@
-//nolint:wsl
+//nolint:wsl,goerr113
 package handler_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/Ras96/traq-kinano-cli/ent"
 	"github.com/Ras96/traq-kinano-cli/interfaces/handler"
+	"github.com/Ras96/traq-kinano-cli/usecases/repository"
 	"github.com/Ras96/traq-kinano-cli/usecases/repository/mock_repository"
 	"github.com/Ras96/traq-kinano-cli/usecases/service"
 	"github.com/Ras96/traq-kinano-cli/util/assert"
 	"github.com/Ras96/traq-kinano-cli/util/random"
+	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
@@ -45,6 +48,18 @@ func Test_handlers_CallAlias(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "ErrRepoCall",
+			args: args{
+				ctx:   context.Background(),
+				short: "test",
+			},
+			want: nil,
+			setup: func(r *mock_repository.MockRepositories, s service.Services, args args, want *ent.Alias) {
+				r.EXPECT().CallAlias(args.ctx, args.short).Return(nil, errors.New("error calling alias"))
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
@@ -58,6 +73,63 @@ func Test_handlers_CallAlias(t *testing.T) {
 			got, err := h.CallAlias(tt.args.ctx, tt.args.short)
 			assert.Error(t, tt.wantErr, err)
 			assert.Equal(t, tt.want, got, cmpopts.IgnoreUnexported(ent.Alias{}))
+		})
+	}
+}
+
+func Test_handlers_AddAlias(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		ctx    context.Context
+		userID uuid.UUID
+		short  string
+		long   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		setup   func(r *mock_repository.MockRepositories, s service.Services, args args)
+		wantErr bool
+	}{
+		{
+			name: "Success",
+			args: args{
+				ctx:    context.Background(),
+				userID: random.UUID(),
+				short:  "test",
+				long:   "testtest",
+			},
+			setup: func(r *mock_repository.MockRepositories, s service.Services, args args) {
+				aargs := repository.NewAddAliasArgs(args.userID, args.short, args.long)
+				r.EXPECT().AddAlias(args.ctx, aargs).Return(&ent.Alias{}, nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "ErrRepoAdd",
+			args: args{
+				ctx:    context.Background(),
+				userID: random.UUID(),
+				short:  "test",
+				long:   "testtest",
+			},
+			setup: func(r *mock_repository.MockRepositories, s service.Services, args args) {
+				aargs := repository.NewAddAliasArgs(args.userID, args.short, args.long)
+				r.EXPECT().AddAlias(args.ctx, aargs).Return(nil, errors.New("error adding alias"))
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+			repo := mock_repository.NewMockRepositories(ctrl)
+			srv := service.NewServices()
+			tt.setup(repo, srv, tt.args)
+			h := handler.NewHandlers(repo, srv)
+			assert.Error(t, tt.wantErr, h.AddAlias(tt.args.ctx, tt.args.userID, tt.args.short, tt.args.long))
 		})
 	}
 }
